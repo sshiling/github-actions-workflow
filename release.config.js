d38edbdc469f81c5dc16c306fb7bc5381e7a1052
sshiling <s.shilingov@gmail.com>
2020-06-13 19:02:49 +0300

    fix: add dist zip

--- release.config.js
+++ release.config.js
@@ -4,6 +4,11 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    "@semantic-release/github", {
+      assets: [
+        { path: "dist.zip", label: "Dist" },
+        { path: "coverage.zip", label: "Coverage" }
+      ]
+    }
   ]
 };
